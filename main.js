6c226326ab5b694b528a1b397de13a3160de3034
leticianakato <137776095+leticianakato@users.noreply.github.com>
2025-05-30 18:46:07 +0000

    decimo segundo commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUV';
+const letrasMinusculas = 'abcdefghijklmnopqrstuv';
 const numeros = '0123456789';
-const simbolos = '!@%*?';
+const simbolos = '!@%*?$+';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
